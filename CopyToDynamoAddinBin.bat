e07b099276e6ad9c430bb68270b6fda5adf3bbc6
Shannon <sh4nnongoh@gmail.com>
2016-04-29 18:45:13 +0800

    Updated Batch file

--- CopyToDynamoAddinBin.bat
+++ CopyToDynamoAddinBin.bat
@@ -6,10 +6,12 @@ IF /I "%1"=="Debug" set OPT_CONFIGURATION=Debug
 
 set OPT_Platform=x64
 
+robocopy %cwd%\extern %cwd%\DynamoAddin_bin *.png /E
+
 if %OPT_CONFIGURATION% equ Debug (
-  robocopy %cwd%\src\cpp_bin\%OPT_Platform%\%OPT_CONFIGURATION% %cwd%\DynamoAddin_bin *.dll *.lib *.pdb /E
+  robocopy %cwd%\src\cpp_bin\%OPT_Platform%\%OPT_CONFIGURATION% %cwd%\DynamoAddin_bin *.dll *.lib *.pdb *.manifest /E
 )
 if %OPT_CONFIGURATION% equ Release (
-  robocopy %cwd%\src\cpp_bin\%OPT_Platform%\%OPT_CONFIGURATION% %cwd%\DynamoAddin_bin *.dll *.lib /E
+  robocopy %cwd%\src\cpp_bin\%OPT_Platform%\%OPT_CONFIGURATION% %cwd%\DynamoAddin_bin *.dll *.lib *.manifest /E
 )
 pause
